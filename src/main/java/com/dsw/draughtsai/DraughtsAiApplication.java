7733b52a9d08d27437cbc1159c5c554ec8d1f9b0
Tendai Zhou <tendaidzhou@gmail.com>
2020-02-12 16:04:17 +0200

    inittial commit

---
+++ src/main/java/com/dsw/draughtsai/DraughtsAiApplication.java
@@ -0,0 +1,13 @@
+package com.dsw.draughtsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraughtsAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DraughtsAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsw/draughtsai/DraughtsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsw.draughtsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraughtsAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
